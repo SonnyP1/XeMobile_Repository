84f8a55d2e0aea2ac77c3512832b917decd11578
Ponce Sr <sjponce@student.uiwtx.edu>
2022-01-24 13:14:54 -0600

    Added camera damping WIP

--- Assets/prefabs/CameraManager/CameraManager.cs
+++ Assets/prefabs/CameraManager/CameraManager.cs
@@ -6,7 +6,10 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] Transform cameraFollowTransform;
     [SerializeField] float RotateSpeed = 20.0f;
+    [SerializeField] [Range(0,1)] float MovementDamping = 0.5f;
+    [SerializeField] [Range(0,1)] float RotateDamping = 0.5f;
     private Camera _mainCamera;
+    private float _time = 0;
 
     void Start()
     {
@@ -21,7 +24,17 @@ public class CameraManager : MonoBehaviour
             transform.Rotate(Vector3.up , RotateSpeed * Time.deltaTime * playerMoveInput.x);
         }
         //makes camera follow
-        _mainCamera.transform.position = cameraFollowTransform.position;
-        _mainCamera.transform.rotation = cameraFollowTransform.rotation;
+        //use lerping to add damping
+
+        Vector3 previousPos = _mainCamera.transform.position;
+        Quaternion previousRot = _mainCamera.transform.rotation;
+
+
+         _mainCamera.transform.position = Vector3.Lerp(previousPos, cameraFollowTransform.position, MovementDamping*Time.deltaTime);
+         _mainCamera.transform.rotation = Quaternion.Lerp(previousRot, cameraFollowTransform.rotation, RotateDamping* Time.deltaTime);
+
+
+        //_mainCamera.transform.position = cameraFollowTransform.position;
+        //_mainCamera.transform.rotation = cameraFollowTransform.rotation;
     }
 }
